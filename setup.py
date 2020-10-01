8902848f7876a3a5a0b31ed2aa4b06ae5b260dbb
marcoEDU <contact@marcobartsch.me>
2020-10-01 23:12:06 +0200

    make setup.py work

--- setup.py
+++ setup.py
@@ -9,97 +9,82 @@ import os
 class Setup():
     def __init__(self,
                  target_device='stripe',
-                 target_ip='192.168.4.1',
-                 webrepl_password='letsglow',
-                 firmware_version='1.13'
                  ):
-        # check if host or strip
-        self.firmware_version = firmware_version
-        self.device = 'computer' if 'files_stripe' in os.listdir() else 'esp'
         self.target_device = target_device
-        self.target_ip = target_ip
-        self.webrepl_password = webrepl_password
+        self.firmware_version = '1.10'
+        self.dev_usb_port = '/dev/tty.usbserial-0001'
 
         self.folders = [
             'files_all_devices',
             'files_host',
             'files_stripe',
             'MicroWebSrv2',
-            'MicroWebSrv2/libs',
-            'MicroWebSrv2/mods',
-            'www',
-            'www/fonts',
-            'www/icons',
-            'www/images',
+            'www'
         ]
 
-    @property
-    def already_executed(self):
-        # check if already executed based on created .txt file
-        return 'setup_completed.txt' in os.listdir()
-
-    def mark_as_executed(self):
-        # create text file to mark script as executed
-        with open('setup_completed.txt', 'w') as fp:
-            pass
-
-    def check_folders(self):
-        # TODO create folder
-        # TODO create __init__.py file inside folder
-        pass
-
-    def check_and_move_files(self):
-        # TODO
-        pass
-
     def install_micropython(self):
-        # TODO
-        pass
+        # search for firmware
+        firmware = [x for x in os.listdir() if x.startswith(
+            'esp32-') and x.endswith(self.firmware_version+'.bin')][0]
+        # install esptool
+        print('Install esptool...')
+        os.system('pip install esptool')
+        # format esp
+        print('Format ESP...')
+        os.system('esptool.py --port {} erase_flash'.format(self.dev_usb_port))
+        # copy firmware
+        print('Copy firmware...')
+        os.system(
+            'esptool.py --chip esp32 --port {} write_flash -z 0x1000 {}'.format(self.dev_usb_port, firmware))
 
-    def save_file_list(self):
-        files = [('files.json', 'files.json')]
+    def install_rshell(self):
+        print('Install rshell for copying files to ESP...')
+        os.system('pip install rshell')
+
+    def sync_files(self):
         for folder in self.folders:
-            files += [(x, folder+'/'+x) for x in os.listdir(folder)
-                      if '.' in x and x != '__init__.py']
+            ignore_folder = {"stripe": "host", "host": "stripe"}
+            if folder != 'files_'+ignore_folder[self.target_device]:
+                print('Sync folder ... ', folder)
+                os.system(
+                    'rshell --port {} rsync -m {} /pyboard/{}'.format(
+                        self.dev_usb_port,
+                        folder,
+                        folder
+                    ))
 
-        with open('files.json', 'w') as outfile:
-            json.dump({
-                "files": files
-            }, outfile)
-
-            print('Saved filelist')
-
-    def upload_files(self):
-        with open('config.json') as json_file:
-            files = json.load(json_file)['files']
-
-            for file_entry in files:
-                ignore_folder = {"stripe": "host", "host": "stripe"}
-                if not 'files_'+ignore_folder[self.target_device] in file_entry[1]:
-                    print('Upload... ', file_entry[1])
-                    os.system(
-                        'python webrepl_cli.py -p {} {} {}:/{}'.format(
-                            self.webrepl_password,
-                            file_entry[1],
-                            self.target_ip,
-                            file_entry[0]
-                        ))
+        print('Add move_files.py')
+        os.system(
+            'rshell --port {} cp move_files.py /pyboard/boot.py'.format(self.dev_usb_port))
 
     def start(self):
-        # check if setup.py is executed on ESP or computer
-        if self.device == 'esp':
-            # check if setup was already executed
-            if self.already_executed == False:
-                self.check_folders()
-                self.check_and_move_files()
-                self.mark_as_executed()
+        started_correct = input(
+            'Did you start this script from an Python3 virtual environment?')
+        if started_correct.lower() == 'y':
+            print('Want to reinstall MicroPython on your ESP32?')
+            install_micropython = input('Enter "y" if yes, else "n".')
+            if install_micropython.lower() == 'y':
+                firmware_version = input(
+                    'What firmware version do you want to install? Press Enter for 1.10')
+                if firmware_version != '':
+                    self.firmware_version = firmware_version
+                dev_usb_port = input(
+                    'Connect your ESP to your computer. Once done: What is the /dev port? Press enter for /dev/tty.usbserial-0001')
+                if dev_usb_port != '':
+                    self.dev_usb_port = dev_usb_port
+                self.install_micropython()
+                print(
+                    'Please press the hard reset button on your ESP32 to continue.')
+                done = input('Done? Press Enter to continue')
+
+            self.install_rshell()
+            print('Collecting and uploading files...')
+            self.sync_files()
+            print('Synced all files to ESP.')
+            os.system('screen {} 115200'.format(self.dev_usb_port))
         else:
-            self.install_micropython()
-            # self.setup_webrepl()
-            # self.save_file_list()
-            # self.upload_files()
-            # os.remove('files.json')
-            # print('Copied all files to ESP. Please restart ESP to finish the setup.')
+            print(
+                'Please create a virtual python environment based on Python3 first and start the script from there.')
 
 
 Setup().start()
