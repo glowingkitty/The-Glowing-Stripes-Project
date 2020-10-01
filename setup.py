7d3b7565ca4ade929f5293fdc659cee7f0673460
marcoEDU <contact@marcobartsch.me>
2020-10-02 00:46:31 +0200

    add separate copy_file and sync_folder function

--- setup.py
+++ setup.py
@@ -7,10 +7,8 @@ import os
 
 
 class Setup():
-    def __init__(self,
-                 target_device='stripe',
-                 ):
-        self.target_device = target_device
+    def __init__(self):
+        self.target_device = 'stripe'
         self.firmware_version = '1.13'
         self.dev_usb_port = '/dev/tty.usbserial-0001'
 
@@ -41,27 +39,38 @@ class Setup():
         print('Install rshell for copying files to ESP...')
         os.system('pip install rshell')
 
+    def sync_folder(self, folder):
+        os.system(
+            'rshell --port {} rsync -m {} /pyboard/{}'.format(
+                self.dev_usb_port,
+                folder,
+                folder
+            ))
+
+    def copy_file(self, from_path, to_path):
+        os.system('rshell --port {} cp {} /pyboard/{}'.format(
+            self.dev_usb_port,
+            from_path,
+            to_path
+        ))
+
     def sync_files(self):
         for folder in self.folders:
             ignore_folder = {"stripe": "host", "host": "stripe"}
             if folder != 'files_'+ignore_folder[self.target_device]:
                 print('Sync folder ... ', folder)
-                os.system(
-                    'rshell --port {} rsync -m {} /pyboard/{}'.format(
-                        self.dev_usb_port,
-                        folder,
-                        folder
-                    ))
+                self.sync_folder(folder)
 
-        os.system(
-            'rshell --port {} cp files_{}/boot.py /pyboard/boot.py'.format(
-                self.dev_usb_port,
-                self.target_device))
+        self.copy_file(
+            'files_{}/boot.py'.format(self.target_device), 'boot.py')
 
     def start(self):
         started_correct = input(
             'Did you start this script from an Python3 virtual environment?')
         if started_correct.lower() == 'y':
+            target = input('Enter target device. Default "stripe"')
+            if target != '':
+                self.target_device = target
             print('Want to reinstall MicroPython on your ESP32?')
             install_micropython = input('Enter "y" if yes, else "n".')
             if install_micropython.lower() == 'y':
@@ -85,4 +94,5 @@ class Setup():
                 'Please create a virtual python environment based on Python3 first and start the script from there.')
 
 
-Setup().start()
+if __name__ == '__main__':
+    Setup().start()
