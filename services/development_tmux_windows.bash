4436ed372a872134171094d6188f9a3def915ddb
marcoEDU <contact@marcobartsch.me>
2020-11-14 21:56:09 +0100

    updated requirements and made development easier with auto tmux session start

---
+++ services/development_tmux_windows.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+session_name=developmentwindows
+cmds=("htop"
+"sudo /home/host/the-glowing-stripes-project/pyvenv/bin/python /home/host/the-glowing-stripes-project/start_animation.py"
+"sudo /home/host/the-glowing-stripes-project/pyvenv/bin/python /home/host/the-glowing-stripes-project/manage.py runserver 0.0.0.0:80"
+"sudo /home/host/the-glowing-stripes-project/pyvenv/bin/python /home/host/the-glowing-stripes-project/connect_to_host.py"
+)
+
+if [ "`tmux ls | grep $session_name`" ]; then
+	echo "The service already started!" >&2
+	exit 1
+fi
+
+# set screen size to be bigger, otherwise cannot contain too many panes
+tmux new-session -s $session_name -d -x 320 -y 80
+
+for i in `seq 0 $[${#cmds[*]}-1]`; do
+	sleep 0.5
+	tmux split-window
+	sleep 0.5
+	tmux select-layout tile
+	sleep 0.5
+	tmux send-keys -l "${cmds[i]}"
+	sleep 0.5
+	tmux send-keys Enter
+done
