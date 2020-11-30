20693bf8005ce0ca18ac074653469ea19964e195
glowinkitty <contact.marcoedu@gmail.com>
2020-11-30 15:25:07 +0100

    added direction field

--- control_center/static/animation_customizer_fields/direction.js
+++ control_center/static/animation_customizer_fields/direction.js
@@ -1,10 +1,60 @@
 let DirectionCustomizer = class {
-    get_direction_field(){
-
+    get default_selected(){
+        if ('start' in animation_customizer.updated_animation['customization'] && animation_customizer.updated_animation['customization']['start']){
+            return animation_customizer.updated_animation['customization']['start']
+        } else {
+            return null
+        }
+        
     }
 
-    change_direction_select(){
+    get possible_directions(){
+        if ('possible_directions' in animation_customizer.updated_animation['customization'] && animation_customizer.updated_animation['customization']['possible_directions']){
+            var directions = animation_customizer.updated_animation['customization']['possible_directions']
+            var directions_output = []
+            if (directions.indexOf('start') >= 0){
+                directions_output.push({
+                    'name':'Bottom to top',
+                    'value':'start'
+                })
+            }
+            if (directions.indexOf('end') >= 0){
+                directions_output.push({
+                    'name':'Top to bottom',
+                    'value':'end'
+                })
+            }
+            if (directions.indexOf('start + end') >= 0){
+                directions_output.push({
+                    'name':'From both sides',
+                    'value':'start + end'
+                })
+            }
+            if (directions.indexOf('center') >= 0){
+                directions_output.push({
+                    'name':'From center',
+                    'value':'center'
+                })
+            }
+            return directions_output
+        } else {
+            return null
+        }
+        
+    }
 
+    get_direction_field(){
+        this.field = new CustomizerField(
+            'Direction',
+            this.possible_directions,
+            this.default_selected,
+            'direction_customizer.change_direction_select(this.value)'
+        )
+        return this.field.get_select_field()
+    }
+
+    change_direction_select(new_selected){
+        this.default_selected = new_selected
     }
 }
 
