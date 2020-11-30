2d4ca8d20a5d747108af3d1483c2e4c31d74c4f6
glowinkitty <contact.marcoedu@gmail.com>
2020-11-30 16:15:34 +0100

    added brightness

--- control_center/static/animation_customizer_fields/brightness.js
+++ control_center/static/animation_customizer_fields/brightness.js
@@ -1,10 +1,25 @@
 let BrightnessCustomizer = class {
-    get_brightness_field(){
-
+    get default_selected(){
+        if ('brightness' in animation_customizer.updated_animation['customization'] && animation_customizer.updated_animation['customization']['brightness']){
+            return animation_customizer.updated_animation['customization']['brightness']
+        } else {
+            return null
+        }
+        
     }
 
-    change_brightness_select(){
+    get_brightness_field(){
+        this.field = new CustomizerField(
+            'Brightness',
+            [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+            this.default_selected,
+            'brightness_customizer.change_brightness_select(this.value)'
+        )
+        return this.field.get_select_field()
+    }
 
+    change_brightness_select(new_selected){
+        this.default_selected = new_selected
     }
 }
 
