a9f2e9b9dcdf94783267821bfe0f92a4b71cae81
glowinkitty <contact.marcoedu@gmail.com>
2021-01-09 23:16:52 +0100

    try adding color transition animation

--- esp32_code/src/neopixel.cpp
+++ esp32_code/src/neopixel.cpp
@@ -2,23 +2,26 @@
 #include <vector>
 #include <string>
 using namespace std;
+#include <math.h>
 
 
 // #define ANALOG_PIN 32
 int num_pin = 22;
 int num_leds = 60;
+int duration_ms = 1000;
+int pause_ms = 0;
+
 
 Adafruit_NeoPixel leds(num_leds, num_pin, NEO_GRB + NEO_KHZ800);
 
 
 string previous_animation;
-string new_animation = "beats";
+string new_animation = "transition";
 
 int num_random_colors = 5;
 int counter_current_color = 0;
 vector<vector<int>> rgb_colors;
-int loudness;
-int previous_loudness;
+
 
 void generate_random_colors(){
     // define random rgb colors here, loop over them and create new random colors if mode is changing 
@@ -40,27 +43,102 @@ boolean animation_has_changed(){
     }
 }
 
+//////////////////////////////////////
+// ANIMATIONS
+//////////////////////////////////////
 void beats(){
     Serial.println("Glow beats...");
-    // TODO add duration, pause
-    leds.clear();
+    // TODO make duration & pause 100 accurate, by calculating also required time for calculation
+    int delay_step = ((duration_ms/num_leds)/2);
+
     for(int i=0; i<num_leds; i++) {
         leds.setPixelColor(i, leds.Color(rgb_colors[counter_current_color][0],rgb_colors[counter_current_color][1],rgb_colors[counter_current_color][2]));
         leds.show();
-        delay(10);
+        delay(delay_step);
     }
     for(int i=num_leds; i>=0; i--) {
         leds.setPixelColor(i, leds.Color(0,0,0));
         leds.show();
-        delay(10);
+        delay(delay_step);
     }
 }
+
+void transition(){
+    Serial.println("Glow transition...");
+    // transition from rgb_colors[counter_current_color-1] to rgb_colors[counter_current_color] in 20 steps
+    int num_of_steps = 20;
+
+    int start_r = rgb_colors[counter_current_color-1][0];
+    int start_g = rgb_colors[counter_current_color-1][1];
+    int start_b = rgb_colors[counter_current_color-1][2];
+
+    int target_r = rgb_colors[counter_current_color][0];
+    int target_g = rgb_colors[counter_current_color][1];
+    int target_b = rgb_colors[counter_current_color][2];
+
+    int by_step_change_r = round((target_r-start_r)/num_of_steps);
+    int by_step_change_g = round((target_g-start_g)/num_of_steps);
+    int by_step_change_b = round((target_b-start_b)/num_of_steps);
+
+    if (by_step_change_r < -255 || by_step_change_r > 255){
+        Serial.println(target_r);
+        Serial.println(start_r);
+        Serial.println(target_r-start_r);
+        by_step_change_r = 0;
+    }
+    if (by_step_change_g < -255 || by_step_change_g > 255){
+        Serial.println(target_g);
+        Serial.println(start_g);
+        Serial.println(target_g-start_g);
+        by_step_change_g = 0;
+    }
+    if (by_step_change_b < -255 || by_step_change_b > 255){
+        Serial.println(target_b);
+        Serial.println(start_b);
+        Serial.println(target_b-start_b);
+        by_step_change_b = 0;
+    }
+
+    int delay_step = (duration_ms/num_of_steps);
+
+    
+    for(int i=0; i<num_of_steps; i++) {
+        start_r += by_step_change_r;
+        start_g += by_step_change_g;
+        start_b += by_step_change_b;
+
+        if (start_r>255){
+            start_r = 255;
+        } else if (start_r<0){
+            start_r = 0;
+        }
+        if (start_g>255){
+            start_g = 255;
+        } else if (start_g<0){
+            start_g = 0;
+        }
+        if (start_b>255){
+            start_b = 255;
+        } else if (start_b<0){
+            start_b = 0;
+        }
+
+        leds.fill(leds.Color(start_r,start_g,start_b));
+        leds.show();
+        delay(delay_step);
+    }
+
+}
+
+//////////////////////////////////////
 
 void init_leds(){
     leds.begin();
 }
 
 void glow(){
+
+    leds.clear();
     if (rgb_colors.size()==counter_current_color){
         counter_current_color = 0;
     }
@@ -72,11 +150,15 @@ void glow(){
     // play animation
     if (new_animation == "beats") {
         beats();
+    } else if (new_animation == "transition") {
+        transition();
     } else{
         Serial.println("ERROR: new_animation not found.");
     }
 
     previous_animation = new_animation;
     counter_current_color+=1;
+    
+    delay(pause_ms);
 }
 
