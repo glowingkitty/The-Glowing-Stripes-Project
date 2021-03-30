61627f6573b9b662d97efdf3bf518c68205702da
glowinkitty <contact.marcoedu@gmail.com>
2021-03-31 00:28:15 +0200

    updated 3d printed case, connectors, esp32 cases, script to update software

--- update_all_led_strips.py
+++ update_all_led_strips.py
@@ -4,6 +4,8 @@ import socket
 import time
 from shutil import copyfile
 
+import requests
+
 
 def hostname_resolves(hostname):
     try:
@@ -38,31 +40,65 @@ for filename in [x for x in os.listdir("./stripe_configs") if x.endswith(".json"
         # copy json file to /data/stripe_config.json
         copyfile("./stripe_configs/"+filename, "./esp32_webserver/data/stripe_config.json")
 
-        os.system("cd esp32_webserver && pio run -t uploadfs --upload-port led_strip__"+led_strip_id+"__server.local")
-        time.sleep(10)
+        print("Uploading code to led_strip__"+led_strip_id+"__server.local...")
+        time.sleep(2)
         os.system("cd esp32_webserver && pio run -t upload --upload-port led_strip__"+led_strip_id+"__server.local")
+
+        print("Waiting for LED strip (webserver) to be online again...")
+        while hostname_resolves("led_strip__"+led_strip_id+"__server.local")==0:
+            print("...")
+            time.sleep(1)
+
+        print("Uploading firmware to led_strip__"+led_strip_id+"__server.local...")
+        time.sleep(2)
+        os.system("cd esp32_webserver && pio run -t uploadfs --upload-port led_strip__"+led_strip_id+"__server.local")
+
         success.append(led_strip_name+"' (webserver, "+led_strip_id+")")
         print("LED strip (webserver) '"+led_strip_name+"' ("+led_strip_id+") updated!")
+
+        print("Waiting for LED strip (webserver) to be online again...")
+        while hostname_resolves("led_strip__"+led_strip_id+"__server.local")==0:
+            print("...")
+            time.sleep(1)
+
+        #TODO update config from all devices (new customizations is now "7", not "5"!)
+
+        print("Activating OTA for led_strip__"+led_strip_id+"__leds.local...")
+        os.environ['NO_PROXY'] = "led_strip__"+led_strip_id+"__server.local"
+        response = requests.get("http://led_strip__"+led_strip_id+"__server.local/activate_ota")
+        if response==200:
+            
+            print("LED strip (LEDs) '"+led_strip_name+"' ("+led_strip_id+") online. Start OTA update...")
+            # if yes:
+            # copy json file to /data/stripe_config.json
+            copyfile("./stripe_configs/"+filename, "./esp32_leds/data/stripe_config.json")
+
+            print("Uploading code to led_strip__"+led_strip_id+"__server.local...")
+            time.sleep(2)
+            os.system("cd esp32_leds && pio run -t upload --upload-port led_strip__"+led_strip_id+"__leds.local")
+
+            print("Waiting for LED strip (webserver) to be online again...")
+            while hostname_resolves("led_strip__"+led_strip_id+"__server.local")==0:
+                print("...")
+                time.sleep(1)
+
+            print("Uploading firmware to led_strip__"+led_strip_id+"__leds.local...")
+            time.sleep(2)
+            os.system("cd esp32_leds && pio run -t uploadfs --upload-port led_strip__"+led_strip_id+"__leds.local")
+
+            success.append(led_strip_name+"' (LEDs, "+led_strip_id+")")
+            print("LED strip (LEDs) '"+led_strip_name+"' ("+led_strip_id+") updated!")
+        
+        else:
+            print(response)
+
+
     else:
-        offline.append(led_strip_name+"' (webserver, "+led_strip_id+")")
-        print("LED strip (webserver) '"+led_strip_name+"' ("+led_strip_id+") offline. Skip.")
+        offline.append(led_strip_name+"' ("+led_strip_id+")")
+        print("LED strip '"+led_strip_name+"' ("+led_strip_id+") offline. Skip.")
 
     
-    # check if domain is online (led_strip__<id>__leds.local)
-    if hostname_resolves("led_strip__"+led_strip_id+"__leds.local"):
-        print("LED strip (LEDs) '"+led_strip_name+"' ("+led_strip_id+") online. Start OTA update...")
-        # if yes:
-        # copy json file to /data/stripe_config.json
-        copyfile("./stripe_configs/"+filename, "./esp32_leds/data/stripe_config.json")
-
-        os.system("cd esp32_leds && pio run -t uploadfs --upload-port led_strip__"+led_strip_id+"__server.local")
-        time.sleep(10)
-        os.system("cd esp32_leds && pio run -t upload --upload-port led_strip__"+led_strip_id+"__server.local")
-        success.append(led_strip_name+"' (LEDs, "+led_strip_id+")")
-        print("LED strip (LEDs) '"+led_strip_name+"' ("+led_strip_id+") updated!")
-    else:
-        offline.append(led_strip_name+"' (LEDs, "+led_strip_id+")")
-        print("LED strip (LEDs) '"+led_strip_name+"' ("+led_strip_id+") offline. Skip.")
+    
         
 
 # copy backup file back to /data/stripe_config.json
